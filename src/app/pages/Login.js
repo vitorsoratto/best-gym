e253fb80c127ed3563bdabb5cd15152537dd51fc
Vitor Soratto <sorattoemail@gmail.com>
2024-02-05 15:10:03 -0300

    Refactor login page layout and styles

--- src/app/pages/Login.js
+++ src/app/pages/Login.js
@@ -22,27 +22,35 @@ const Login = () => {
 
   return (
     <div className='login-container'>
-      <h2>Login</h2>
-      <div className='p-fluid'>
-        <div className='p-field'>
-          <label htmlFor='email'>Usuário</label>
-          <InputText
-            id='email'
-            type='text'
-            value={email}
-            onChange={(e) => setemail(e.target.value)}
-          />
+      <div className='login-main'>
+        <h2>Login</h2>
+        <div className='p-fluid'>
+          <div className='p-field'>
+            <label htmlFor='email'>Usuário</label>
+            <InputText
+              id='email'
+              type='text'
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </div>
+          <div className='p-field'>
+            <label htmlFor='password'>Senha</label>
+            <InputText
+              id='password'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button label='Entrar' onClick={handleLogin} />
         </div>
-        <div className='p-field'>
-          <label htmlFor='password'>Senha</label>
-          <InputText
-            id='password'
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <Button label='Entrar' onClick={handleLogin} />
+      </div>
+      <div className="signup-link">
+        <span>
+          Não tem uma conta?&nbsp;
+          <a href="/signup">Registre-se.</a>
+        </span>
       </div>
     </div>
   );
